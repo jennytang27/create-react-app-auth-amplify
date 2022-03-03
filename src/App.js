6ad8388aeb82738a177d1cf7d57c6a7026a65b3f
jennytang27 <100750881+jennytang27@users.noreply.github.com>
2022-03-03 10:12:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,14 @@ import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-/*class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <h1>Hello, {this.props.name}</h1>;
     );
   }
 }
-*/
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
+
+
 export default withAuthenticator(App);
